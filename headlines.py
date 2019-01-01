dfc3f5024a28cbee5e10021353204b775e392d10
jwestgard <jwestgard@mac.com>
2019-01-01 16:38:29 -0500

    use Jinja templates

--- headlines.py
+++ headlines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask
+from flask import Flask, render_template
 import feedparser
 
 RSS_FEEDS = {
@@ -12,47 +12,19 @@ RSS_FEEDS = {
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return   '''<html>
-                    <body>
-                        <h1>Hello World</h1>
-                        <p>This is my project homepage.</p>
-                        <h2>Links</h2>
-                        <ul>
-                            <li><a href="headlines">
-                                Headlines project for learning Flask</a>
-                                </li>
-                            <li><a href="http://terpconnect.umd.edu/~westgard">
-                                Personal homepage</a>
-                                </li>
-                            <li><a href="http://www.github.com/jwestgard">
-                                Coding projects on Github</a>
-                                </li>
-                        </ul>
-                      </body>
-                </html>'''
-
+    return render_template('home.html')
 
 @app.route('/headlines')
 @app.route('/headlines/<source>')
 def get_news(source='at'):
     feed = feedparser.parse(RSS_FEEDS[source])
     article = feed['entries'][0]
-    return   """<html>
-                    <body>
-                        <h1>{0} Headlines</h1>
-                        <b>{1}</b> <br />
-                        <i>{2}</i> <br />
-                        <p>{3}</p> <br />
-                    </body>
-                </html>""".format(source.upper(),
-                                  article.get("title"),
-                                  article.get("published"),
-                                  article.get("summary")
-                                  )
-
+    return   render_template('headlines.html', 
+                             articles=feed['entries'],
+                             source=source.upper()
+                             )
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
